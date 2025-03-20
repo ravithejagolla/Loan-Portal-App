eafccd077cb36f9f3fa5b0ebc84dfd9aa5f4c829
ravithejagolla <gollaravitheja007@gmail.com>
2025-03-20 16:52:37 +0530

    redis changes

--- Server/config/redis.js
+++ Server/config/redis.js
@@ -1,9 +1,15 @@
+import env from 'dotenv'
+env.config()
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: '127.0.0.1',
+  host: process.env.REDIS_USER_NAME,
   port: 6379,
-  
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null, 
+  retryStrategy(times) {
+    return Math.min(times * 50, 2000); 
+  },
 });
 
 export default redis;
